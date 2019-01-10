656fa159bf3db3a609839d99110acbab2f1a719d
Damien Martin-Guillerez <dmarting@google.com>
2019-01-10 10:19:22 +0100

    Add clippy to Travis CI

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if [ "$1" == "format" ]; then
-  echo "Installing rustfmt..."
-  rustup toolchain install nightly
-  rustup component add --toolchain nightly rustfmt-preview
-  which rustfmt || cargo install --force rustfmt-nightly
-  cargo +nightly fmt -- --version
-fi
+case "$1" in
+  format)
+    echo "Installing rustfmt..."
+    rustup component add --toolchain nightly rustfmt-preview
+    which rustfmt || cargo install --force rustfmt-nightly
+    cargo +nightly fmt -- --version
+    ;;
+  clippy)
+    echo "Installing clippy..."
+    rustup component add clippy --toolchain=nightly || cargo install --git https://github.com/rust-lang/rust-clippy/ --force clippy
+    ;;
+esac
