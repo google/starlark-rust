0494712e3e7022df26692bea0967ba7c432ee3c9
Daniel Wagner-Hall <dawagner@gmail.com>
2019-04-28 23:46:29 +0100

    Remove redundant closures

--- starlark/src/values/tuple.rs
+++ starlark/src/values/tuple.rs
@@ -273,7 +273,7 @@ impl TypedValue for Tuple {
     fn to_str(&self) -> String {
         format!(
             "({}{})",
-            self.content.iter().map(|x| x.to_repr(),).enumerate().fold(
+            self.content.iter().map(Value::to_repr).enumerate().fold(
                 "".to_string(),
                 |accum, s| if s.0 == 0 {
                     accum + &s.1
