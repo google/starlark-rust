52e7275c6214c5bd90e45f76fd81c8000c3dc6c6
Mike Stipicevic <stipredirect@gmail.com>
2019-09-11 09:41:58 +0000

    Convert to structopt (#190)

--- starlark-repl/bin/starlark-rust.rs
+++ starlark-repl/bin/starlark-rust.rs
@@ -14,9 +14,10 @@
 
 //! A command line interpreter for Starlark, provide a REPL.
 
+extern crate structopt;
+
 use codemap::CodeMap;
 use codemap_diagnostic::{ColorConfig, Diagnostic, Emitter};
-use getopts::Options;
 use starlark::eval::interactive::{eval, eval_file, EvalError};
 use starlark::stdlib::global_environment_with_extensions;
 use starlark::syntax::ast::AstStatement;
@@ -24,114 +25,97 @@ use starlark::syntax::dialect::Dialect;
 use starlark::syntax::parser::{parse, parse_file};
 use starlark::values::Value;
 use starlark_repl::{print_function, repl};
-use std::env;
 use std::process::exit;
 use std::sync::{Arc, Mutex};
+use structopt::clap::AppSettings;
+use structopt::StructOpt;
 
-const EXIT_CODE_USAGE: i32 = 1;
 const EXIT_CODE_FAILURE: i32 = 2;
 
-macro_rules! print_usage {
-    ($program: expr, $opts: expr) => (
-        {
-            let brief = format!("[Starlark in Rust interpretor]
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "starlark-repl",
+    about = "Starlark in Rust interpreter",
+    global_settings(&[AppSettings::ColoredHelp]),
+)]
+pub struct Opt {
+    #[structopt(short = "a", long, help = "Parse and print AST instead of evaluating.")]
+    ast: bool,
 
-Usage: {} [options] [file1..filen]
-", $program);
-            eprint!("{}", $opts.usage(&brief));
-        }
-    );
-    ($program: expr, $opts: expr, $($fmt:expr),+) => (
-        {
-            print_usage!($program, $opts);
-            eprintln!($($fmt),+);
-        }
-    )
+    #[structopt(
+        short = "b",
+        long,
+        help = concat!(
+            "Parse the build file format instead of full Starlark. See https://docs.rs/starlark/",
+            env!("CARGO_PKG_VERSION"),
+            "/starlark/eval/index.html#build_file",
+        )
+    )]
+    build_file: bool,
+
+    #[structopt(
+        short = "c",
+        help = "Starlark command to run after files have been parsed."
+    )]
+    command: Option<String>,
+
+    #[structopt(
+        short = "r",
+        long,
+        conflicts_with = "command",
+        help = "Run a REPL after files have been parsed."
+    )]
+    repl: bool,
+
+    #[structopt(name = "FILE", help = "Files to interpret")]
+    files: Vec<String>,
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
+    let opt = Opt::from_args();
 
-    let mut opts = Options::new();
-    opts.optflag(
-        "b",
-        "build_file",
-        concat!(
-            "Parse the build file format instead of full Starlark. See https://docs.rs/starlark/",
-            env!("CARGO_PKG_VERSION"),
-            "/starlark/eval/index.html#build_file"
-        ),
-    );
-    opts.optflag("h", "help", "Show the usage of this program.");
-    opts.optflag("r", "repl", "Run a REPL after files have been parsed.");
-    opts.optflag("a", "ast", "Parse and print AST instead of evaluating");
-    opts.optopt(
-        "c",
-        "command",
-        "Starlark command to run after files have been parsed.",
-        "expr",
-    );
-    match opts.parse(&args[1..]) {
-        Err(e) => {
-            print_usage!(program, opts, "\n{}\n", e);
+    let command = opt.command;
+    let ast = opt.ast;
+
+    let global = print_function(global_environment_with_extensions());
+    global.freeze();
+
+    let dialect = if opt.build_file {
+        Dialect::Build
+    } else {
+        Dialect::Bzl
+    };
+    let free_args_empty = opt.files.is_empty();
+    for i in opt.files.into_iter() {
+        if ast {
+            let codemap = Arc::new(Mutex::new(CodeMap::new()));
+            maybe_print_ast_or_exit(parse_file(&codemap, &i, dialect), &codemap);
+        } else {
+            maybe_print_or_exit(eval_file(
+                &i,
+                dialect,
+                &mut global.child(&i),
+                global.clone(),
+            ));
         }
-        Ok(matches) => {
-            if matches.opt_present("h") {
-                print_usage!(program, opts);
-            } else {
-                let build_file = matches.opt_present("b");
-                let opt_repl = matches.opt_present("r");
-                let command = matches.opt_str("c");
-                let ast = matches.opt_present("a");
-
-                if opt_repl && command.is_some() {
-                    eprintln!("Cannot pass both -r and -c");
-                    exit(EXIT_CODE_USAGE);
-                }
-
-                let global = print_function(global_environment_with_extensions());
-                global.freeze();
-
-                let dialect = if build_file {
-                    Dialect::Build
-                } else {
-                    Dialect::Bzl
-                };
-                let free_args_empty = matches.free.is_empty();
-                for i in matches.free.into_iter() {
-                    if ast {
-                        let codemap = Arc::new(Mutex::new(CodeMap::new()));
-                        maybe_print_ast_or_exit(parse_file(&codemap, &i, dialect), &codemap);
-                    } else {
-                        maybe_print_or_exit(eval_file(
-                            &i,
-                            dialect,
-                            &mut global.child(&i),
-                            global.clone(),
-                        ));
-                    }
-                }
-                if opt_repl || (free_args_empty && command.is_none()) {
-                    println!("Welcome to Starlark REPL, press Ctrl+D to exit.");
-                    repl(&global, dialect, ast);
-                }
-                if let Some(command) = command {
-                    let path = "[command flag]";
-                    if ast {
-                        let codemap = Arc::new(Mutex::new(CodeMap::new()));
-                        maybe_print_ast_or_exit(parse(&codemap, path, &command, dialect), &codemap);
-                    } else {
-                        maybe_print_or_exit(eval(
-                            "[command flag]",
-                            &command,
-                            dialect,
-                            &mut global.child("[command flag]"),
-                            global.clone(),
-                        ));
-                    }
-                }
-            }
+    }
+    if opt.repl || (free_args_empty && command.is_none()) {
+        println!("Welcome to Starlark REPL, press Ctrl+D to exit.");
+        repl(&global, dialect, ast);
+    }
+    if let Some(command) = command {
+        let path = "[command flag]";
+        if ast {
+            let codemap = Arc::new(Mutex::new(CodeMap::new()));
+            maybe_print_ast_or_exit(parse(&codemap, path, &command, dialect), &codemap);
+        } else {
+            maybe_print_or_exit(eval(
+                "[command flag]",
+                &command,
+                dialect,
+                &mut global.child("[command flag]"),
+                global.clone(),
+            ));
         }
     }
 }
